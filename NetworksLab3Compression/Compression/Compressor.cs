ad35593d7ff316cf2488bd88ec569136da8bbb8a
Clay <clayton.simons90@gmail.com>
2014-11-29 17:51:38 -0800

    Revert "Possibly have LZ code"
    This reverts commit 44f391ddd164b9053b818347203436f366f45b83.
    
    Conflicts:
    	NetworksLab3Compression/Compression/Compresser.cs
    	NetworksLab3Compression/Compression/Compression.csproj
    	NetworksLab3Compression/Compression/Compressor.cs
    	NetworksLab3Compression/Compression/HuffmanCompressor.cs
    	NetworksLab3Compression/Compression/bin/Debug/Compression.dll
    	NetworksLab3Compression/Compression/bin/Debug/Compression.pdb
    
    	NetworksLab3Compression/Compression/obj/Debug/Compression.csproj.FileListAbsolute.txt
    	NetworksLab3Compression/Compression/obj/Debug/Compression.dll
    	NetworksLab3Compression/Compression/obj/Debug/Compression.pdb
    	NetworksLab3Compression/NetworksLab3Compression.v12.suo
    	NetworksLab3Compression/NetworksLab3Compression/ClientForm.cs
    
    	NetworksLab3Compression/NetworksLab3Compression/bin/Debug/Compression.dll
    
    	NetworksLab3Compression/NetworksLab3Compression/bin/Debug/Compression.pdb
    
    	NetworksLab3Compression/NetworksLab3Compression/bin/Debug/NetworksLab3Compression.exe
    
    	NetworksLab3Compression/NetworksLab3Compression/bin/Debug/NetworksLab3Compression.pdb
    
    	NetworksLab3Compression/NetworksLab3Compression/obj/Debug/NetworksLab3Compression.csprojResolveAssemblyReference.cache
    
    	NetworksLab3Compression/NetworksLab3Compression/obj/Debug/NetworksLab3Compression.exe
    
    	NetworksLab3Compression/NetworksLab3Compression/obj/Debug/NetworksLab3Compression.pdb
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/bin/Debug/Compression.dll
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/bin/Debug/Compression.pdb
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/bin/Debug/NetworksLab3CompressionServer.exe
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/bin/Debug/NetworksLab3CompressionServer.pdb
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/obj/Debug/NetworksLab3CompressionServer.csprojResolveAssemblyReference.cache
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/obj/Debug/NetworksLab3CompressionServer.exe
    
    	NetworksLab3Compression/NetworksLab3CompressionServer/obj/Debug/NetworksLab3CompressionServer.pdb

---
+++ NetworksLab3Compression/Compression/Compressor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compression
+{
+    public class Compressor : System.MarshalByRefObject
+    {
+        Compresser comp;
+        public Compressor()
+        {
+            comp = null;
+        }
+        public bool setHuff()
+        {
+            bool answer = false;
+            if (comp == null || comp.GetType() != typeof(HuffmanCompressor))
+            {
+                comp = new HuffmanCompressor();
+            }
+            if (comp != null)
+                answer = true;
+            return answer;
+        }
+        public BitArray Compress(byte[] data, String name)
+        {
+            BitArray answer = null;
+            if(comp != null)
+                answer = comp.Compress(data, name);
+            return answer;
+        }
+        public byte[] Decompress(BitArray data, String name)
+        {
+            byte[] answer = null;
+            if(comp != null)
+                answer = comp.Decompress(data, name);
+            return answer;
+        }
+    }
+}
